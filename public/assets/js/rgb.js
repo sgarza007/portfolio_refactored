3d81504977976deca2061a0c5245bfcbabfa947b
Saul Garza <saulgarza@Sauls-MacBook-Pro.local>
2018-12-11 14:04:42 -0800

    base of RGB guesser. Able to pick random RGB to guess and display when guessed correctly

--- public/assets/js/rgb.js
+++ public/assets/js/rgb.js
@@ -1,26 +1,44 @@
 function main(){
     let squares = $(".square");
-    // squares.on("click", function () {
-    //     console.log("Clicked");
-    // });
-    // squares.each(function(i){
-    //     $(this).css("color",randomRGB);
-    // });
-
-    squares.each(function(i,square){
-        $(this).css( "backgroundColor",randomRGB);
-    })
+    //assign a random RGB to each square
+    setColor(squares);
+    //pick an rgb to be the one to guess
+    let correctRGB = $(squares[randomNum(6)]).css("backgroundColor");
+    console.log(correctRGB);
+    //check to see if they guessed the right RGB
+    squares.on("click", function () {
+        let clickedRGB = $(this).css("backgroundColor");
+        //check to see if right one was guessed
+        if(clickedRGB === correctRGB){
+            //turn every square to the correct color
+            squares.each(function(){
+                $(this).css( "backgroundColor",correctRGB);
+            });
+            //change text to say play again?
+        } else{
+            //make the square disappear
+            console.log("wrong");
+            $(this).css( "backgroundColor","#2C3E50");
+        }
+    });
 }
 
+//sets color of squares
+function setColor(squares){
+    squares.each(function(){
+        $(this).css( "backgroundColor",randomRGB());
+    });
+}
 
 //create a random RGB generator
 function randomRGB(){
-    let r = randomNum();
-    let g = randomNum();
-    let b = randomNum();
+    let r = randomNum(256);
+    let g = randomNum(256);
+    let b = randomNum(256);
     return "rgb("+r+", "+g+", "+b+")";
 }
 
-function randomNum(){
-    return Math.floor(Math.random()*256);
+//return a randomNumber between 0 and 255
+function randomNum(num){
+    return Math.floor(Math.random()*num);
 }
