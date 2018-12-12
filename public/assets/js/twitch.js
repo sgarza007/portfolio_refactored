f0461179841a34473353b6bb020e21e7cb4fd547
Saul Garza <saulgarza@Sauls-MacBook-Pro.local>
2018-12-11 19:04:02 -0800

    added fetch functionality so that you can make a call to the twitch API

--- public/assets/js/twitch.js
+++ public/assets/js/twitch.js
@@ -1,5 +1,20 @@
 function twitchMain(){
-    fetch("https://api.twitch.tv/kraken/streams/requiemslaps?client_id=03i44s2mzxk0s9ksqzvhu16i5im3ul")
+    let getDataBtn = $("#getData");
+    let streamName = $("#streamName");
+    getDataBtn.on("click",function(){
+        //check to see if there was anything typed
+
+        //TODO: ERROR HANDLING
+        if(streamName.val.length === 0) console.log("error");
+        console.log(streamName.val());
+        makeRequest(streamName.val());
+    });   
+}
+
+function makeRequest(streamName){
+    const key = "?client_id=03i44s2mzxk0s9ksqzvhu16i5im3ul";
+    const url = "https://api.twitch.tv/kraken/streams/";
+    fetch(url+streamName+key)
     .then(function(res){
         if(!res.ok){
             throw Error("Oops! Something went wrong.");
