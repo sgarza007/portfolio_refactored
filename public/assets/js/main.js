80b4ce208ce92b12e6ab832659385aecc7c0c601
Saul Garza <saulgarza@Sauls-MacBook-Pro.local>
2018-12-13 16:03:56 -0800

    added the travel section to the thing

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,21 @@
 function main(){
-    var waypoints = $('#skill').waypoint(function(direction) {
-        notify(this.element.id + ' hit 25% from top of window') 
-      }, {
-        offset: '25%'
-      }) 
+    // var waypoints = $('#skill').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window') 
+    //   }, {
+    //     offset: '25%'
+    //   });
+
+      initMasonry();
+}
+
+function initMasonry(){
+  $('.grid').masonry({
+    itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+
+  $('.grid').imagesLoaded().progress( function() {
+    $('.grid').masonry('layout');
+  });
 }
